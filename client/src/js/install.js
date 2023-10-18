4a736c8153e1795c52bd8a8e1bb18f473402bbd5
Kris Kurzawa <kurzawa1@yahoo.com>
2023-10-18 15:14:35 -0400

    unnecessary console.logs and comments deleted

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,7 +1,5 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     console.log('hit')
     console.log('event' + event)
@@ -10,7 +8,6 @@ window.addEventListener('beforeinstallprompt', (event) => {
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
     console.log(promptEvent)
@@ -22,7 +19,6 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log('install hit');
     butInstall.classList.toggle('hidden', true);
